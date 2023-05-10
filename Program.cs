42fee8817d887242684c10a7c7afdfbad98d16ce
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-05-09 23:57:59 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string S;
+
+
+            do
+            {  
+                Console.WriteLine("Digite se sexo (F para feminino e M para masculino): ");
+            S = Console.ReadLine();
+        }
+            while ((S != "F")&(S != "M")) ;
+
+        }
+    }
+}
